7cebb40982ad4dab70099b42d19a9dde407bad3b
Toba0z <tobgabby@aol.com>
2024-03-05 09:20:40 -0800

    confirmed reading from json file

--- routes/apiRouters.js
+++ routes/apiRouters.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 //Get all videso Data 
 router.get("/", (req, res)=>{
 
-    const videosDataShortVersion = fs.readFileSync("./Data/videos.json");
+    const videosDataShortVersion = fs.readFileSync("./Data/video-details.json");
     const videosJson = JSON.parse(videosDataShortVersion);
     res.status(200).send(videosJson);
     console.log("login video data test",videosJson);
